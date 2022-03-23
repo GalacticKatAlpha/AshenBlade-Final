88f89b2fafbf1dd9fc59afe5a83d4900af3bd69f
GalacticKatAlpha <524721@chrome.escg.ac.uk>
2022-03-23 19:47:22 +0000

    Added Healthbar

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    int damage = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider dmg)
+    {
+        dmg.gameObject.GetComponent<Health>().TakeDamage(damage);
+    }
+}
